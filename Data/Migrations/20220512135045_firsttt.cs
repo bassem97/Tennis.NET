675b2a709c4d2d613003992c28032dc0fdd8aed4
BASSEM <bassem.jadoui@esprit.tn>
2022-05-12 15:03:43 +0100

    ssss

---
+++ Data/Migrations/20220512135045_firsttt.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class firsttt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pays",
+                columns: table => new
+                {
+                    CodePays = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Monnaie = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pays", x => x.CodePays);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tournoi",
+                columns: table => new
+                {
+                    NoTour = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Coef = table.Column<double>(type: "float", nullable: false),
+                    Dotation = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournoi", x => x.NoTour);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Joueur",
+                columns: table => new
+                {
+                    JoueurId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PaysFK = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joueur", x => x.JoueurId);
+                    table.ForeignKey(
+                        name: "FK_Joueur_Pays_PaysFK",
+                        column: x => x.PaysFK,
+                        principalTable: "Pays",
+                        principalColumn: "CodePays",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jouer",
+                columns: table => new
+                {
+                    JoueurFK = table.Column<int>(type: "int", nullable: false),
+                    TournoiFK = table.Column<int>(type: "int", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jouer", x => new { x.JoueurFK, x.TournoiFK });
+                    table.ForeignKey(
+                        name: "FK_Jouer_Joueur_JoueurFK",
+                        column: x => x.JoueurFK,
+                        principalTable: "Joueur",
+                        principalColumn: "JoueurId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Jouer_Tournoi_TournoiFK",
+                        column: x => x.TournoiFK,
+                        principalTable: "Tournoi",
+                        principalColumn: "NoTour",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jouer_TournoiFK",
+                table: "Jouer",
+                column: "TournoiFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Joueur_PaysFK",
+                table: "Joueur",
+                column: "PaysFK");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jouer");
+
+            migrationBuilder.DropTable(
+                name: "Joueur");
+
+            migrationBuilder.DropTable(
+                name: "Tournoi");
+
+            migrationBuilder.DropTable(
+                name: "Pays");
+        }
+    }
+}
---
+++ Web/Controllers/JoueurController.cs
@@ -0,0 +1,65 @@
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Service.Joueur;
+using Service.Pays;
+
+namespace Web.Controllers
+{
+    public class JoueurController : Controller
+    {
+        private readonly IJoueurService _joueurService;
+        private readonly IPaysService _paysService;
+
+        public JoueurController(IJoueurService joueurService,IPaysService paysService)
+        {
+            _joueurService = joueurService;
+            _paysService = paysService;
+        }
+
+        // GET: Joueur
+        public IActionResult Index()
+        {
+            List<Joueur> listJoueurs = _joueurService.GetMany().ToList();
+
+            return View(listJoueurs);
+        }
+
+      
+
+        // GET: Joueur/Create
+        public ActionResult Create()
+        {
+            
+            List<Pays> payss = _paysService.GetMany().ToList();
+            ViewBag.payss = new SelectList(payss, "CodePays", "Nom");
+            return View();
+        }
+
+        // POST: Joueur/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Joueur joueur, IFormFile ImageFile)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads",ImageFile.FileName);
+            Stream stream = new FileStream(path, FileMode.Create);
+            ImageFile.CopyTo(stream);
+            joueur.Photo = ImageFile.FileName;
+            _joueurService.Add(joueur);
+            _joueurService.Commit();
+            return RedirectToAction(nameof(Index));
+       
+          
+        }
+
+   
+       
+    }
+}
